a0fb5047bb9cb68811e1249122f5c427f187ba23
Abhishek Bhargav <39580132+abhibhargav29@users.noreply.github.com>
2020-07-04 17:55:04 +0530

    Update Complex.py

--- Complex.py
+++ Complex.py
@@ -5,18 +5,21 @@ class Complex(object):
         self.real = real
         self.img = imaginary
         
+    #This function method object's __add__ method overloading + operator for this class
     def __add__(self, no):
         ans=Complex()
         ans.real = self.real+no.real
         ans.img = self.img+no.img 
         return(ans)
-
+    
+    #This method overrides object's __sub__ method overloading - operator for this class
     def __sub__(self, no):
         ans=Complex()
         ans.real = self.real-no.real
         ans.img = self.img-no.img 
         return(ans)
 
+    #This method overrides object's __mul__ method overloading * operator for this class
     def __mul__(self, no):
         ans=Complex()
         a = self.real
@@ -27,6 +30,7 @@ class Complex(object):
         ans.img = (a*d)+(b*c)
         return(ans)
 
+    #This method overrides object's __add__ method overloading % operator for this class
     def __truediv__(self, no):
         conj = Complex(no.real, -(no.img))
         denominator = (no.__mul__(conj)).real
@@ -36,6 +40,7 @@ class Complex(object):
         ans.img = numerator.img/denominator
         return(ans)
 
+    #This method returns the modulus of complex number
     def mod(self):
         ans=Complex()
         ans.real = math.sqrt(self.real**2 + self.img**2)
@@ -56,8 +61,15 @@ class Complex(object):
         return result
 
 
-c = map(float, input().split())
-d = map(float, input().split())
+print("Enter numbers as real part followed by imaginary part")
+c = map(float, input("Enter first number: ").split())
+d = map(float, input("Enter second number: ").split())
 x = Complex(*c)
 y = Complex(*d)
-print(*map(str, [x+y, x-y, x*y, x/y, x.mod(), y.mod()]), sep='\n')
+print("\n")
+print("The sum is: "+str(x+y))
+print("The subtraction is: "+str(x-y))
+print("The multiplication is: "+str(x*y))
+print("The division is: "+str(x/y))
+print("Mod of x: ",str(x.mod()))
+print("Mod of y: ",str(y.mod()))
